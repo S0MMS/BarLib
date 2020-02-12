83a0db8d957361d7c461c8bc1a512187fc55e811
Chris Shreve <cshreve@rei.com>
2020-02-12 10:34:37 -0800

    first commit

---
+++ BarLib/Classes/MyUtil.swift
@@ -0,0 +1,12 @@
+//
+//  MyUtil.swift
+//  BarLib
+//
+//  Created by Chris Shreve on 2/12/20.
+//
+
+import UIKit
+
+class MyUtil: NSObject {
+
+}
--- README.md
+++ README.md
@@ -27,3 +27,4 @@ S0MMS, cshreve@rei.com
 ## License
 
 BarLib is available under the MIT license. See the LICENSE file for more info.
+# BarLib
